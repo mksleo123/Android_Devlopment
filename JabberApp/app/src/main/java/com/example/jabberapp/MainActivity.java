8d0a90cf4f9316408948440069002e1b6694a3b9
Mayur kumar <jgmmkslksmgwn@gmail.com>
2021-06-01 00:50:34 +0530

    jabbber app1.0.

--- JabberApp/app/src/main/java/com/example/jabberapp/MainActivity.java
+++ JabberApp/app/src/main/java/com/example/jabberapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.jabberapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.jabberapp.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth= FirebaseAuth.getInstance();
+
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.settings:
+                break;
+            case R.id.Logout:
+                break;
+        }
+        return true;
     }
 }
